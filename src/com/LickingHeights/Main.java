5d8dd31fa3303ade96b2e58e679d815951243d54
Jason Bordeau <jason.bordeau.2021@student.lhschools.org>
2020-02-24 12:22:47 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Jason");
+        System.out.println("I worked all summer.");
+        System.out.println("My friends and I went to the movies a couple times.");
+        System.out.println("We also played a lot of video games and board games.");
+        //Print three more lines about what you did last summer
+
+    }
+}
